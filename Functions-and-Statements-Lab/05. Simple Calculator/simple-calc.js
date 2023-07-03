fd6768b934328d8bc7987a3c28649b2d066a6298
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-03 16:10:40 +0300

    Update JS Functions ARROW FUNCIONS IN OBJECT
    Correct way to use object for holding some functions.

--- Functions-and-Statements-Lab/05. Simple Calculator/simple-calc.js
+++ Functions-and-Statements-Lab/05. Simple Calculator/simple-calc.js
@@ -1,33 +1,15 @@
 // With nasted function and if\switch statemants
 
-const multiply = (x, y) => x * y;
-
-const divide = (num1, num2) => num1 / num2;
-
-const add = (num1, num2) => num1 + num2;
-
-const substract = (num1, num2) => num1 - num2;
+const calculator = {
+  multiply: (x, y) => x * y,
+  divide: (num1, num2) => num1 / num2,
+  add: (num1, num2) => num1 + num2,
+  substract: (num1, num2) => num1 - num2,
+};
 
 function calculate(num1, num2, operator) {
-  if (operator === "multiply") {
-    return multiply(num1, num2);
-  }
-
-  if (operator === "divide") {
-    return divide(num1, num2);
-  }
-
-  if (operator === "add") {
-    return add(num1, num2);
-  }
-
-  if (operator === "substract") {
-    return substract(num1, num2);
-  }
+  return calculator[operator](num1, num2);
 }
 
 const result = calculate(5, 5, "multiply");
 console.log(result);
-// Arrow function
-
-// NO IF\SWITCH !
