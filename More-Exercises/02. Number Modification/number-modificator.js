2d75ab90c2a6995bbe53394c587d65c1853faea9
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-05 17:38:45 +0300

    Update number-modificator.js
    Commit with help-function.

--- More-Exercises/02. Number Modification/number-modificator.js
+++ More-Exercises/02. Number Modification/number-modificator.js
@@ -1,18 +1,19 @@
-function solve(num) {
-  num = num.toString();
-  let average =
+function calculateAverage(num) {
+  return (
     num
       .split("")
       .map(Number)
-      .reduce((total, curr) => total + curr, 0) / num.length;
+      .reduce((total, curr) => total + curr, 0) / num.lenght
+  );
+}
+
+function solve(num) {
+  num = num.toString();
+  let average = calculateAverage(num);
 
   while (average < 5) {
     num += "9";
-    average =
-      num
-        .split("")
-        .map(Number)
-        .reduce((total, curr) => total + curr, 0) / num.length;
+    average = calculateAverage(num);
   }
 
   return num;
