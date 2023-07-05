ae91796c8df18e6c8aceef079075b8b0f2cfe0cc
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-05 17:32:41 +0300

    Updated JS Functions More-exercise with NumberGenerator
    Commit without help-function.

---
+++ More-Exercises/02. Number Modification/number-modificator.js
@@ -0,0 +1,21 @@
+function solve(num) {
+  num = num.toString();
+  let average =
+    num
+      .split("")
+      .map(Number)
+      .reduce((total, curr) => total + curr, 0) / num.length;
+
+  while (average < 5) {
+    num += "9";
+    average =
+      num
+        .split("")
+        .map(Number)
+        .reduce((total, curr) => total + curr, 0) / num.length;
+  }
+
+  return num;
+}
+
+console.log(solve(5835));
