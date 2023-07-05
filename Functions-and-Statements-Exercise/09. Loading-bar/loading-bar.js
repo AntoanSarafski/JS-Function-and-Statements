1c05b51d5d046af1d147ef52c096d0f60edc185b
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-05 09:01:25 +0300

    Create loading-bar.js
    Commit with 09.Loading-bar.

---
+++ Functions-and-Statements-Exercise/09. Loading-bar/loading-bar.js
@@ -0,0 +1,20 @@
+function printLoadingBar(percentage) {
+  let bar = "";
+  for (let index = 1; index <= 100; index += 10) {
+    if (index < percentage) {
+      bar += "%";
+    } else {
+      bar += ".";
+    }
+  }
+
+  if (percentage === 100) {
+    console.log("100% Complete!");
+    console.log(`[${bar}]`);
+  } else {
+    console.log(`${percentage}% [${bar}]`);
+    console.log("Still loading...");
+  }
+}
+
+printLoadingBar(10);
