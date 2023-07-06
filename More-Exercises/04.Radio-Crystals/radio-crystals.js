e01b65b4e40459734320957804a7ae22b7a55020
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-07-06 15:53:56 +0300

    Update radio-crystals.js
    Commit our app with functionality to work correct with more than one crystals.

--- More-Exercises/04.Radio-Crystals/radio-crystals.js
+++ More-Exercises/04.Radio-Crystals/radio-crystals.js
@@ -1,64 +1,66 @@
-function solve(target, chunk) {
-  while (chunk > target) {
-    // CUT ...
-    if (chunk / 4 >= target) {
-      let counter = 0;
-      while (chunk / 4 >= target) {
-        chunk /= 4;
-        counter++;
+function solve(input) {
+  const [target, ...chunks] = input;
+  for (let chunk of chunks) {
+    while (chunk > target) {
+      // CUT ...
+      if (chunk / 4 >= target) {
+        let counter = 0;
+        while (chunk / 4 >= target) {
+          chunk /= 4;
+          counter++;
+        }
+        chunk = Math.floor(chunk);
+        console.log(`Cut x${counter}`);
+        console.log("Transporting and washing");
+        continue;
       }
-      chunk = Math.floor(chunk);
-      console.log(`Cut x${counter}`);
-      console.log("Transporting and washing");
-      continue;
+
+      // Lap ...
+      if (chunk - chunk * 0.2 >= target) {
+        let counter = 0;
+        while (chunk - chunk * 0.2 >= target) {
+          chunk -= chunk * 0.2;
+          counter++;
+        }
+        chunk = Math.floor(chunk);
+        console.log(`Lap x${counter}`);
+        console.log("Transporting and washing");
+        continue;
+      }
+      // Grind...
+      if (chunk - 20 >= target) {
+        let counter = 0;
+        while (chunk - 20 >= target) {
+          chunk -= 20;
+          counter++;
+        }
+        chunk = Math.floor(chunk);
+        console.log(`Grind x${counter}`);
+        console.log("Transporting and washing");
+        continue;
+      }
+
+      //Etch
+      if (chunk - 2 >= target - 1) {
+        let counter = 0;
+        while (chunk - 2 >= target - 1) {
+          chunk -= 2;
+          counter++;
+        }
+        chunk = Math.floor(chunk);
+        console.log(`Etch x${counter}`);
+        console.log("Transporting and washing");
+
+        continue;
+      }
     }
 
-    // Lap ...
-    if (chunk - chunk * 0.2 >= target) {
-      let counter = 0;
-      while (chunk - chunk * 0.2 >= target) {
-        chunk -= chunk * 0.2;
-        counter++;
-      }
-      chunk = Math.floor(chunk);
-      console.log(`Lap x${counter}`);
-      console.log("Transporting and washing");
-      continue;
+    if (chunk + 1 === target) {
+      chunk += 1;
+      console.log("X-ray x1");
     }
-    // Grind...
-    if (chunk - 20 >= target) {
-      let counter = 0;
-      while (chunk - 20 >= target) {
-        chunk -= 20;
-        counter++;
-      }
-      chunk = Math.floor(chunk);
-      console.log(`Grind x${counter}`);
-      console.log("Transporting and washing");
-      continue;
-    }
-
-    //Etch
-    if (chunk - 2 >= target - 1) {
-      let counter = 0;
-      while (chunk - 2 >= target - 1) {
-        chunk -= 2;
-        counter++;
-      }
-      chunk = Math.floor(chunk);
-      console.log(`Etch x${counter}`);
-      console.log("Transporting and washing");
-
-      continue;
-    }
+    console.log(`Finished crystal ${target} microns`);
   }
-
-  if (chunk + 1 === target) {
-    chunk += 1;
-    console.log("X-ray x1");
-  }
-  console.log(`Finished crystal ${target} microns`);
-
-  return chunk;
 }
-solve(1375, 50000);
+
+solve([1000, 4000, 8100]);
